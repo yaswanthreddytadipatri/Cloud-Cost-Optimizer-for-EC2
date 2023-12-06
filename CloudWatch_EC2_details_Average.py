c315993a346be82e30aee3b61e8ede30ae9e96fe
yaswanthreddytadipatri <tysreddy@gmail.com>
2023-12-06 15:37:47 +0530

    Modified the algorithm

--- CloudWatch_EC2_details_Average.py
+++ CloudWatch_EC2_details_Average.py
@@ -85,9 +85,9 @@ print("File is saved as cpu_utilization_metrics_data.json")
 
 
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn import metrics
+# from sklearn.model_selection import train_test_split
+# from sklearn.linear_model import LinearRegression
+# from sklearn import metrics
 import matplotlib.pyplot as plt
 
 
